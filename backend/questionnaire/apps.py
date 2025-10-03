5cfcd79c899c5b35f5bab8bf8b00931c3e6502e7
HarisNvr <www.haris@bk.ru>
2025-10-03 12:28:50 +0300

    alter names

--- backend/questionnaire/apps.py
+++ backend/questionnaire/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class QuestionnaireConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "questionnaire"
+    verbose_name = "Раздел «ОПРОСЫ»"
